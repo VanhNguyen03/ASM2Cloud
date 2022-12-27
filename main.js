b3c0f64cc4730d6ed0728a0ad2589623964dfc0b
VanhNguyen03 <anhnvgch210312@fpt.edu.vn>
2022-12-27 08:00:29 +0700

    all CRUD... with CSS

---
+++ main.js
@@ -0,0 +1,60 @@
+var express = require('express')
+var app = express()
+
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
+
+const { insertNewProduct, getAllProducts, deleteProductById, updateProduct, findProductById } = require('./databaseHandler');
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.get('/new',(req,res)=>{
+    res.render('add')
+})
+
+app.post('/new', async (req, res) => {
+    const name = req.body.txtProductName
+    const price = req.body.txtProductPrice
+    const picUrl = req.body.txtProductPic
+    const newProduct = { 
+        name: name, 
+        price: Number.parseFloat(price),
+        picture: picUrl
+    }
+    await insertNewProduct(newProduct)
+    res.redirect('/')
+})
+
+app.get('/all',async (req,res)=>{
+    let results = await getAllProducts()
+    res.render('view',{results:results})
+})
+
+app.get('/deleteProduct', async (req, res) => {
+    const id = req.query.id
+    await deleteProductById(id)
+    res.redirect('/all')
+})
+
+app.get('/editProduct', async (req, res) => {
+    const id = req.query.id
+    const productToEdit = await findProductById(id)
+    res.render("edit", {product: productToEdit})
+})
+
+app.post('/editProduct', async (req, res) => {
+    const id = req.body.id
+    const name = req.body.txtProductName
+    const price = req.body.txtProductPrice
+    const picUrl = req.body.txtProductPic
+    await updateProduct(id, name, price, picUrl)
+    res.redirect('/all')
+})  
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, (req, res) => {
+    console.log("Server is running at PORT: ", PORT)
+})
